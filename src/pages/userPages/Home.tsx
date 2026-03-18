a4d416d0c1acfbc1f5e71b944d8b32fd26baa275
shibin <shibin>
2026-03-18 18:15:14 +0530

    unwanted reference removed

--- src/pages/userPages/Home.tsx
+++ src/pages/userPages/Home.tsx
@@ -1,26 +1,21 @@
 import Hero from '../../components/userComponents/Hero';
 import FeaturedCars from '../../components/userComponents/FeaturedCars';
 import Features from '../../components/userComponents/Features';
-import StorySection from '../../components/userComponents/StorySection';
-// import EnhancedParallax from '../../components/userComponents/EnhancedParallax';
+
 import TrustIndicators from '../../components/userComponents/TrustIndicators';
 import B2BPartnership from '../../components/userComponents/B2BPartnership';
 import GlobalBackground from '../../components/userComponents/GlobalBackground';
 import ContactSection from '../../components/userComponents/ContactSection';
 import ComparisonSection from '../../components/userComponents/ComparisonSection';
-import TrustSlider from '../../components/userComponents/TrustSlider';
+
 
 const Home = () => {
   return (
     <div className="w-full relative">
       <GlobalBackground />
       <Hero />
-      {/* <TrustSlider /> */}
-      {/* <StorySection /> */}
       <ComparisonSection />
       <Features />
-      {/* <EnhancedParallax /> */}
-      {/* <TechSpecs /> */}
       <FeaturedCars />
       <TrustIndicators />
       <B2BPartnership />
