36b7882f8633ea933255ccf7b0845fd6f5b7435d
shibin <shibin>
2026-03-19 17:22:28 +0530

    vercel error fix

--- src/pages/userPages/Home.tsx
+++ src/pages/userPages/Home.tsx
@@ -1,6 +1,5 @@
 import Hero from '../../components/userComponents/Hero';
 import FeaturedCars from '../../components/userComponents/FeaturedCars';
-import Features from '../../components/userComponents/Features';
 import VerificationTimeline from '../../components/userComponents/VerificationTimeline';  
 import B2BPartnership from '../../components/userComponents/B2BPartnership';
 import GlobalBackground from '../../components/userComponents/GlobalBackground';
